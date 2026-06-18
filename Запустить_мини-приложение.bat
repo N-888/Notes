17de083f6bd24febdfbc5a3d753e3002bb577eda
N-888 <mamambell@gmail.com>
2026-06-18 06:17:58 +0300

    10. Файлы для запуска приложений в один клик (Windows)

---
+++ Запустить_мини-приложение.bat
@@ -0,0 +1,10 @@
+@echo off
+chcp 65001 >nul
+echo.
+echo ==========================================
+echo   ЗАПУСК: mini_app (main.py)
+echo ==========================================
+echo.
+cd mini_app
+python main.py
+pause
